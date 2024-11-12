bce4f6cded62bc5281de656d4e04c0bbfc9656bb
Lescy48 <162001748+Lescy48@users.noreply.github.com>
2024-11-12 23:17:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleLogoutMenu() {
+    const logoutMenu = document.getElementById("logoutMenu");
+    logoutMenu.style.display = logoutMenu.style.display === "block" ? "none" : "block";
+  }
+  
